2603ee5246e1e8024db0c46f0646df7d577a1b25
jazper balatbat <jazperbalatbat@gmail.com>
2020-09-11 21:01:35 +0800

    new project react js

---
+++ src/navBar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../src/App.css';
+
+function Navbar({ name }) {
+    return (
+        <a className="navBtn" href="">{name}</a>
+    );
+}
+
+export default Navbar;
