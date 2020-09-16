15daeaf2f17326b310832a52b0951656b5748246
jazper balatbat <jazperbalatbat@gmail.com>
2020-09-16 17:54:03 +0800

    added menu

--- src/App.js
+++ src/App.js
@@ -29,6 +29,38 @@ const App = () => {
         <Menucard path="/images/sushi/sushi1.jpg" title="Assorted Sushi" />
         <Menucard path="/images/sushi/sushi1.jpg" title="Cabbage Siumai" />
         <Menucard path="/images/sushi/sushi1.jpg" title="Salmon Sushi" />
+        {/* sample */}
+
+        <Menucard path="/images/burger/burger1.jpg" title="Classic Burger" />
+        <Menucard path="/images/burger/burger1.jpg" title="Breakfast Burger" />
+        <Menucard path="/images/burger/burger1.jpg" title="Burger Combo" />
+        <Menucard path="/images/pasta/pasta1.jpg" title="White Pasta" />
+        <Menucard path="/images/pasta/pasta1.jpg" title="Tomato Pasta" />
+        <Menucard path="/images/pasta/pasta1.jpg" title="Bean Pasta" />
+        <Menucard path="/images/pizza/pizza1.jpg" title="Pepperoni Pizza" />
+        <Menucard path="/images/pizza/pizza1.jpg" title="Organic Pizza" />
+        <Menucard path="/images/pizza/pizza1.jpg" title="Olive pizza" />
+        <Menucard path="/images/ramen/ramen1.jpg" title="CharSiu Ramen" />
+        <Menucard path="/images/ramen/ramen1.jpg" title="Thai Ramen" />
+        <Menucard path="/images/ramen/ramen1.jpg" title="Sho Ramen" />
+        <Menucard path="/images/sushi/sushi1.jpg" title="Assorted Sushi" />
+        <Menucard path="/images/sushi/sushi1.jpg" title="Cabbage Siumai" />
+        <Menucard path="/images/sushi/sushi1.jpg" title="Salmon Sushi" />
+        <Menucard path="/images/burger/burger1.jpg" title="Classic Burger" />
+        <Menucard path="/images/burger/burger1.jpg" title="Breakfast Burger" />
+        <Menucard path="/images/burger/burger1.jpg" title="Burger Combo" />
+        <Menucard path="/images/pasta/pasta1.jpg" title="White Pasta" />
+        <Menucard path="/images/pasta/pasta1.jpg" title="Tomato Pasta" />
+        <Menucard path="/images/pasta/pasta1.jpg" title="Bean Pasta" />
+        <Menucard path="/images/pizza/pizza1.jpg" title="Pepperoni Pizza" />
+        <Menucard path="/images/pizza/pizza1.jpg" title="Organic Pizza" />
+        <Menucard path="/images/pizza/pizza1.jpg" title="Olive pizza" />
+        <Menucard path="/images/ramen/ramen1.jpg" title="CharSiu Ramen" />
+        <Menucard path="/images/ramen/ramen1.jpg" title="Thai Ramen" />
+        <Menucard path="/images/ramen/ramen1.jpg" title="Sho Ramen" />
+        <Menucard path="/images/sushi/sushi1.jpg" title="Assorted Sushi" />
+        <Menucard path="/images/sushi/sushi1.jpg" title="Cabbage Siumai" />
+        <Menucard path="/images/sushi/sushi1.jpg" title="Salmon Sushi" />
 
       </div>
     </div>
