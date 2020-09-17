f0c7c27c598af18f14af25bbbbef191c1161a944
jazper balatbat <jazperbalatbat@gmail.com>
2020-09-17 20:15:56 +0800

    change coding style to components

--- src/menu.js
+++ src/menu.js
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function Menucard({ path, title }) {
-    return (
-        <div className="menu-card">
-            <div className="img-card">
-                <img src={path}></img></div>
-            <h1>{title}</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum dolores ipsam non.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum dolores ipsam non.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum dolores ipsam non.
-            </p>
-            <a href="">order now</a>
-        </div>
-    );
+
+class Menucard extends Component {
+    render() {
+        console.log(this.props.menu);
+        return this.props.menu.map((item) => (
+            <div key={item.id} className="menu__card">
+                <img src={item.path} />
+                <h1>{item.title}</h1>
+            </div>
+        ));
+
+    }
 }
+
+// function Menucard({ path, title }) {
+//     return (
+//         <div className="menu-card">
+//             <div className="img-card">
+//                 <img src={path}></img></div>
+//             <h1>{title}</h1>
+//             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum dolores ipsam non.
+//             Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum dolores ipsam non.
+//             Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum dolores ipsam non.
+//             </p>
+//             <a href="">order now</a>
+//         </div>
+//     );
+// }
 
 export default Menucard;
